24aa31ae9b33b16c4839f79406170b8ee2a49d78
Clay Karwisch <clay.karwisch@gmail.com>
2021-03-01 11:07:40 -0500

    add about and contact pages:
      - add controller actions for about and contact
      - add views for about and contact pages
      - update layout to dynamically include the page's title
      - add tests for the existence of the static pages and the correct titles

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,6 +1,10 @@
 require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @app_name = "Flitter"
+  end
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
@@ -14,5 +18,30 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get about" do
     get static_pages_about_url
     assert_response :success
+  end
+
+  test "should get contact" do
+    get static_pages_contact_url
+    assert_response :success
+  end
+
+  test "should get title of home page" do
+    get static_pages_home_url
+    assert_select "title", "Home | #{@app_name}"
+  end
+
+  test "should get title of help page" do
+    get static_pages_help_url
+    assert_select "title", "Help | #{@app_name}"
+  end
+
+  test "should get title of about page" do
+    get static_pages_about_url
+    assert_select "title", "About | #{@app_name}"
+  end
+
+  test "should get title of contact page" do
+    get static_pages_contact_url
+    assert_select "title", "Contact | #{@app_name}"
   end
 end
