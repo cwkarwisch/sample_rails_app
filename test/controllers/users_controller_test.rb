fa951e88f7feb564ddd8a2e0796f1f47f1d99308
Clay Karwisch <clay.karwisch@gmail.com>
2021-03-12 11:41:13 -0500

    implement user signup

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -3,6 +3,7 @@ require "test_helper"
 class UsersControllerTest < ActionDispatch::IntegrationTest
   def setup
     @app_name = "Flitter"
+    @user = User.first
   end
 
   test "should get new" do
@@ -14,4 +15,9 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     get signup_path
     assert_select "title", "Sign up | #{@app_name}"
   end
+
+  # test "should get show" do
+  #   get "/users/#{@user.id}"
+  #   assert_response :success
+  # end
 end
